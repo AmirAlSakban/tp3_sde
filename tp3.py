27936a3a38c73ae65e77c7c9cc3188d4dc0db94a
AmirAlSakban <amir.al@stud.fils.upb.ro>
2022-04-05 09:55:47 +0300

    Create tp3.py
    ex1,2,3

---
+++ tp3.py
@@ -0,0 +1,54 @@
+"""
+var1 = 12
+var2 = 17.9
+var3 = "SdE2"
+tip1 = type(var1)
+tip2 = type(var2)
+tip3 = type(var3)
+print (var1, var2, var3)
+
+print(tip1, tip2, tip3)
+
+n = input("write value of n")
+print("n equals" + n)
+
+var=1
+primList =[]
+while var <=n:
+    var4 = 2
+    while var4 < var:
+        if var % var4 = 0
+        var4 +=1
+        else primList.append(var)
+
+        break
+    break
+    var+=1
+
+    break
+
+    print(primList)
+    """
+
+fruitList = []
+fruitList.append('mar')
+fruitList.append('banana')
+fruitList.append('capsuna')
+fruitList.append('pepene')
+fruitList.append('kiwi')
+
+fruitList.sort()
+lungime=1
+
+while lungime < len(fruitList)+1:
+    f=fruitList(lungime)
+    print(f+ len(f))
+    lungime +=1
+  
+
+print(fruitList, len(fruitList(lungime)))
+fruitList.append('rosu')
+fruitList.append('verde')
+fruitList.append('galben')
+reversed_fruitList = fruitList[::-1]
+print(reversed_string)
